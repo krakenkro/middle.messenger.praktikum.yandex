6b82fd584712066467cdcf962a70032ce3b9f685
krakenkro <sekimovab@gmail.com>
2024-03-26 19:11:56 +0600

    Настраиваю eslint и stylelint

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
 		outDir: resolve(__dirname, 'dist'),
 	},
 	server: {
-		port: 3000
+		port: 3000,
 	},
 	css: {
 		preprocessorOptions: {
 			scss: {
-				additionalData: `@import "./src/assets/scss/global.scss";`
-			}
-		}
-	}
+				additionalData: '@import "./src/assets/scss/global.scss";',
+			},
+		},
+	},
 });
